e670cd2152b4dd81dc7b7cb4d446c21f23953118
DESKTOP-T90CO9O\Administrator <meijh@fosun.com>
2019-09-11 13:30:07 +0800

    test

--- sendMail.js
+++ sendMail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer')
 
-let transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   // host: 'smtp.ethereal.email',
   service: 'qq', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
   // port: 465, // SMTP 端口
@@ -13,13 +13,12 @@ let transporter = nodemailer.createTransport({
 })
 
 function sendMail(message) {
-  let mailOptions = {
+  const mailOptions = {
     from: '"1107334911" <1107334911@qq.com>', // 发送地址
     to: '1107334911@qq.com', // 接收者
     subject: '部署通知', // 主题
     html: message // 内容主体
   }
-  // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       return console.log(error)
--- webhook.js
+++ webhook.js
@@ -1,58 +1,56 @@
-let http = require('http')
-let crypto = require('crypto')
-let { spawn } = require('child_process')
-let SECRET = '123456'
-let sendMail = require('./sendMail')
+const http = require('http')
+const crypto = require('crypto')
+const { spawn } = require('child_process')
+const sendMail = require('./sendMail')
+
 function sign(body) {
-  return (
-    `sha1=` +
-    crypto
-      .createHmac('sha1', SECRET)
-      .update(body)
-      .digest('hex')
-  )
+  const SECRET = '123456'
+  return 'sha1=' + crypto.createHmac('sha1', SECRET).update(body).digest('hex')
 }
-let server = http.createServer(function(req, res) {
+
+const server = http.createServer((req, res) => {
   console.log(req.method, req.url)
   if (req.method == 'POST' && req.url == '/webhook') {
-    let buffers = []
-    req.on('data', function(buffer) {
+    const buffers = []
+    req.on('data', buffer => {
       buffers.push(buffer)
     })
-    req.on('end', function(buffer) {
-      let body = Buffer.concat(buffers)
-      let event = req.headers['x-github-event'] //event=push
-      //github请求来的时候，要传递请求体body,另外还会传一个signature过来，你需要验证签名对不对
-      let signature = req.headers['x-hub-signature']
-      if (signature !== sign(body)) {
-        return res.end('Not Allowed')
-      }
+
+    req.on('end', buffer => {
+      const body = Buffer.concat(buffers)
+      // github发送请求进来，要传递请求体body，还会传一个signature，需验证签名正确与否
+      const { 'x-github-event': event, 'x-hub-signature': signature } = req.headers
+      if (signature !== sign(body)) return res.end('Not Allowed')
       res.setHeader('Content-Type', 'application/json')
       res.end(JSON.stringify({ ok: true }))
-      if (event == 'push') {
-        //开始布署
-        let payload = JSON.parse(body)
-        console.log('开始布署了', payload.repository.name)
-        let child = spawn('sh', [`./${payload.repository.name}.sh`])
-        let buffers = []
-        child.stdout.on('data', function(buffer) {
-          buffers.push(buffer)
-        })
-        child.stdout.on('end', function(buffer) {
-          let logs = Buffer.concat(buffers).toString()
-          sendMail(`<h1>部署日期: ${new Date()}</h1>
-            <h2>部署人: ${payload.pusher.name}</h2>
-            <h2>部署邮箱: ${payload.pusher.email}</h2>
-            <h2>提交信息: ${payload.head_commit && payload.head_commit['message']}</h2>
-            <h2>布署日志: ${logs.replace('\r\n', '<br/>')}</h2>
-          `)
-        })
-      }
+      if (event !== 'push') return
+      // 开始布署
+      const {
+        repository: { name: repoName } = {},
+        pusher: { name: pusherName, email: pusherEmail } = {},
+        head_commit: { message: commitMsg } = {}
+      } = JSON.parse(body)
+      const child = spawn('sh', [`./${repoName}.sh`])
+      const buffers = []
+      child.stdout.on('data', (buffer) => {
+        buffers.push(buffer)
+      })
+      child.stdout.on('end', (buffer) => {
+        const logs = Buffer.concat(buffers).toString()
+        sendMail(
+         `<h1>部署日期: ${new Date()}</h1>
+          <h2>部署人: ${pusherName}</h2>
+          <h2>部署邮箱: ${pusherEmail}</h2>
+          <h2>提交信息: ${commitMsg}</h2>
+          <h2>布署日志: ${logs.replace('\r\n', '<br/>')}</h2>`
+        )
+      })
     })
   } else {
     res.end('Not Found')
   }
 })
+
 server.listen(4000, () => {
-  console.log('webhook服务已经在4000端口上启动')
+  console.log('webhook服务已在4000端口启动')
 })
